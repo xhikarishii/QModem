ecf9e25bc4bdf3e5268603ce1e999b8d51cebd36
sfwtw <wtw@cr.cx>
2025-05-16 23:29:40 +0800

    luci-app-qmodem: add Huawei MH50000-31 support

--- luci/luci-app-qmodem/root/usr/share/qmodem/vendor/huawei.sh
+++ luci/luci-app-qmodem/root/usr/share/qmodem/vendor/huawei.sh
@@ -244,15 +244,12 @@ function base_info(){
 
 cell_info()
 {
-
-
     at_command="AT^MONSC"
     response=$(at $at_port $at_command | grep "\^MONSC:" | sed 's/\^MONSC: //')
     
     local rat=$(echo "$response" | awk -F',' '{print $1}')
-
     case $rat in
-        "NR")
+        "NR"|"NR-5GC")
             network_mode="NR5G-SA Mode"
             nr_mcc=$(echo "$response" | awk -F',' '{print $2}')
             nr_mnc=$(echo "$response" | awk -F',' '{print $3}')
